f07465c9b8992b07451572f1586daafca31a1e90
Aashir Razzaq <aashir.razzaq10@gmail.com>
2024-02-23 19:57:53 +0500

    added basic rotation on the bird

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BirdScript : MonoBehaviour
 {
@@ -9,6 +12,11 @@ public class BirdScript : MonoBehaviour
     public LogicScript logic;
     public float flapStrength;
     public bool isAlive = true;
+    public float dropRotationSpeed;
+    public float flapRotationSpeed;
+    private float _timer = 0.6f;
+    private bool _run;
+    private float _y;
 
     private AudioSource flapAudio;
     // Start is called before the first frame update
@@ -16,6 +24,7 @@ public class BirdScript : MonoBehaviour
     {
         flapAudio = GetComponent<AudioSource>();
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        _y = transform.position.y;
     }
 
     // Update is called once per frame
@@ -25,7 +34,30 @@ public class BirdScript : MonoBehaviour
         {
             rigidbody.velocity = Vector2.up * flapStrength;
             flapAudio.Play();
+            _run = false;
+            _timer = 0;
         }
+
+        if (_y > transform.position.y && _run)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(new Vector3(0, 0, -60)),
+            dropRotationSpeed * Time.deltaTime);
+        }
+        else if(_y < transform.position.y)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 40), flapRotationSpeed * Time.deltaTime);
+        }
+        _y = transform.position.y;
+
+        if (_timer < 0.8f)
+        {
+            _timer += Time.deltaTime;
+        }
+        else
+        {
+            _run = true;
+        }
+        Debug.Log(_timer);
     }
 
     public void OnCollisionEnter2D(Collision2D other)
